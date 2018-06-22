08860fb8bb130dc13a5e78c786215e83703c94fb
Pooja Ande <pooja.ande18@gmail.com>
2018-06-22 15:14:17 +0530

    Angular Assignment-2

---
+++ assignment2/app3/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  Marvellous Infosystems <input class="app3" type="text" >`,
+  styles:['app3 { background-color : #0C9458; color: #1F97B1; border-style: none } ']
+})
+export class AppComponent {
+  title = 'app';
+}
